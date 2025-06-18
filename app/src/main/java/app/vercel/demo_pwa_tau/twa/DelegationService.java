1daa5eedd587b006d9a7be008d20fdd72775cd40
rimaakter1 <rimac201262@gmail.com>
2025-06-18 14:52:30 +0600

    added

---
+++ app/src/main/java/app/vercel/demo_pwa_tau/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.demo_pwa_tau.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
